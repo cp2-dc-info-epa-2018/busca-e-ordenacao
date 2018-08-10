e8b0755448144789ac167e72f5de9d80d35f1e05
Ygor Canalli <ygor.canalli@gmail.com>
2018-08-10 11:23:45 -0300

    definindo funcao leitura de arquivo e bubble sort

--- main.c
+++ main.c
@@ -10,31 +10,48 @@ void imprime_vetor(int V[], int n)
 	printf("\n");
 }
 
-int busca_linear(int V[], int chave)
+void vetor_de_arquivo(int n, int V[n], char* caminho)
+{
+  int i;
+  
+  FILE *f;
+  f = fopen(caminho, "r");
+  
+  for (i = 0; i < n; ++i)
+  {
+    if (!fscanf(f, "%d", &V[i])) 
+		break;
+     
+  }
+
+  fclose(f);
+
+}
+
+int busca_linear(int n, int V[n], int chave)
 {
 	return -1;
 }
 
-int busca_binaria(int V[], int chave)
+int busca_binaria(int n, int V[n], int chave)
 {
 	return -1;
+}
+
+void bubble_sort(int n, int V[n])
+{
+	
 }
 
 int main()
 {
-	int V[11];
-	V[0] = 1;
-	V[1] = 3;
-	V[2] = 4;
-	V[3] = 6;
-	V[4] = 7;
-	V[5] = 8;
-	V[6] = 9;
-	V[7] = 15;
-	V[8] = 19;
-	V[9] = 22;
-	V[10] = 29;
-
-	imprime_vetor(V, 11);
+	int n = 10;
+	int V[n];
+	
+	vetor_de_arquivo(n, V, "i10.txt");
+	imprime_vetor(V, n);
+	bubble_sort(n, V);
+	imprime_vetor(V, n);
+	
 	return 0;
 }
